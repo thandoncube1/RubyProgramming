946a676af0cd6283ecc8e678aa45ba7e891f3d0a
thandon263 <ncubethando20@gmail.com>
2024-02-20 15:47:55 -0500

    Adding the instance variables to the list of variable

--- tutorial-basics/variables.rb
+++ tutorial-basics/variables.rb
@@ -14,3 +14,29 @@ undefined local variable or method `x' for main:Object (NameError)
 Variable inside block: 1
 =end
 
+# Instance Variables
+
+=begin
+Instance Variables start with the @ symbol and are accessible across
+different methods within the instance. They persist as long as the
+object instance exists.
+=end
+
+class User
+    def initialize(first_name:, last_name:)
+        # Define instance variables
+        @first_name = first_name
+        @last_name = last_name
+    end
+
+    def full_name
+        # You can use your instance variables here
+        @first_name + " " + @last_name
+    end
+end
+
+# Initialize user
+user = User.new(first_name: "John", last_name: "Doe")
+
+# Return full name
+user.full_name # => "John Doe"
